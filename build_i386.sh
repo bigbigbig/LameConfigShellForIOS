bc0af19729be8b4479ea8c1e2606f010388805a2
Mingsheng Zhang <1046724509@qq.com>
2020-06-21 19:51:45 +0800

    解决找不到目录及找不到iphoneossdk的错误

--- build_i386.sh
+++ build_i386.sh
@@ -1,36 +1,56 @@
-./configure \
+path="/Users/zhangmingsheng/Workspace/影音/交叉编译/Lame/lame-3.99.5"
+target="/thin/armv7"
+cd $path
+#使用-p参数创建路径，有则创建，无则不创建
+sudo mkdir -m 755 -p $target
+#指定xcode路径，防止后面使用xcrun -sdk iphoneos报找不到sdk的错误
+sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer/
 
-# 通常是GNU标准中关闭动态链接库的选项
+./configure \
 --disable-shared \
+--disable-frontend \
+--host=arm-apple-darwin \
+--prefix=$path$target \
+CC="xcrun -sdk iphoneos clang -arch i386" \
+CFLAGS="-arch armv7 -fembed-bitcode -miphoneos-version-min=7.0" \
+LDFLAGS="-arch armv7 -fembed-bitcode -miphoneos-version-min=7.0"
+
+make clean
+make -j8
+make install
+
+#./configure \
+# 通常是GNU标准中关闭动态链接库的选项
+#--disable-shared \
 
 # Do not build the lame executable default=build
 # 不编译出 LAME 的可执行文件
---disable-frontend \
+#--disable-frontend \
 
 # cross-compile to build programs to run on HOST [BUILD]
 # 指定库最终要运行的平台
---host=arm-apple-darwin \
+#--host=arm-apple-darwin \
 
 # By default, `make install' will install all the files in
 # `/usr/local/bin', `/usr/local/lib' etc.  You can specify
 # an installation prefix other than `/usr/local' using `--prefix',
 # for instance `--prefix=$HOME'.
 # 指定编译好的库放在哪个目录
---prefix="./thin/i386" \
+#--prefix="./thin/armv7" \
 
 # C compiler command
 # 指定交叉工具编译链的路径
-CC="xcrun -sdk iphoneos clang -arch i386" \
+#CC="xcrun -sdk iphoneos clang -arch i386" \
 
 # C compiler flags
 # 指定编译所带的参数。
-# 这里指定编译的目标平台是i386，bitcode选项，编译的库最低支持的iOS版本
-CFLAGS="-arch i386 -fembed-bitcode -miphones-version-min=7.0" \
+# 这里指定编译的目标平台是armv7，bitcode选项，编译的库最低支持的iOS版本
+#CFLAGS="-arch armv7 -fembed-bitcode -miphones-version-min=7.0" \
 
 # linker flags, e.g. -L<lib dir> if you have libraries in a nonstandard directory <lib dir>
 # 指定链接过程中的参数，同样也需要带上bitcode选项以及开发者期望app支持的最低iOS版本
-LDFLAGS="-arch i386 -fembed-bitcode -miphones-version-min=7.0"
+#LDFLAGS="-arch armv7 -fembed-bitcode -miphones-version-min=7.0"
 
-make clean
-make -j8
-make install
+#make clean
+#make -j8
+#make install
